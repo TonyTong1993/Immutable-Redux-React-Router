bf2c2d7ea6f056d7532151987807ed30e463830c
tonytong <tongwanhua1993@163.com>
2017-05-27 16:26:56 +0800

    图怕说明

--- React-Router/jump/src/App.js
+++ React-Router/jump/src/App.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router,Route,Link,Switch,Redirect} from 'react-router-dom';
+const About = ()=>{
+  return (
+    <div>
+      <h4>About me</h4>
+    </div>
+    )
+}
+const Home = ()=>{
+  return (
+    <div>
+      <h4>Home</h4>
+      <button>
 
+      </button>
+    </div>
+    )
+}
+const Settings = ()=>{
+  return (
+    <div>
+      <h4>Settings</h4>
+    </div>
+    )
+}
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -13,7 +38,19 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+          <button><Link to="/home">home</Link></button>
+          <button><Link to="/about">about</Link></button>
+          <button><Link to="/settings">settings</Link></button>
+         
+         
+        <Switch>
+           <Route path="/home" component = {Home} />
+           <Route path="/about" component = {About} />
+           <Route path="/settings" component = {Settings} />
+        </Switch>
+
       </div>
+      </Router>
     );
   }
 }
